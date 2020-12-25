682eaaadcf1a27da06769627363b145e89477c64
Mo-banzu <mbandu.ilik@gmail.com>
2020-12-26 04:52:23 +0700

    Delete dreeeeee.py

--- dre/dreeeeee.py
+++
@@ -1,9 +0,0 @@
-"""DREEe dree dreeeee dreee"""
-from itertools import repeat as dreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-dreeeeeeeeeeeeeeeeeeeeeeee = any
-dreeeeeeeee = map
-dreeeeeeeeeeeeeeee = print
-dre = 'dre'
-dreeeeeeeeeeeeeeeeeeeeeeee(dreeeeeeeee(dreeeeeeeeeeeeeeee, dreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee(dre)))
-
-# deeee
